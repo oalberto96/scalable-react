83cc497fb0d7ba4178c6ee4f68cd6feb5f0fef1f
oalberto96 <ochoapublic@gmail.com>
2018-09-09 16:40:29 -0700

    Getting data to navigation component from the store

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -8,10 +8,21 @@ import React from "react";
 
 import styles from "./styles.css";
 
-function Navigation() {
+function Navigation({ topics }) {
   return (
-    <div className={styles.navigation}>This is the navigation component</div>
+    <div className={styles.navigation}>
+      We have {topics.length} in the nav component
+    </div>
   );
 }
+
+Navigation.propTypes = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 export default Navigation;
--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -4,12 +4,25 @@
  *
  */
 
-import { fromJS } from 'immutable';
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import { fromJS } from "immutable";
+import { DEFAULT_ACTION } from "./constants";
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  topics: [
+    {
+      name: "libraries",
+      description: "Links to useful open source libraries"
+    },
+    {
+      name: "apps",
+      description: "Links to new and exciting apps"
+    },
+    {
+      name: "news",
+      description: "links to programming related news articles"
+    }
+  ]
+});
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
