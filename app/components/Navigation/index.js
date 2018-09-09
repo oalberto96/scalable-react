5ba1ea9743de4c7cd095ab698b7d33a4979db9b9
oalberto96 <ochoapublic@gmail.com>
2018-09-09 16:33:29 -0700

    Add navigation component

---
+++ app/components/Navigation/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Navigation
+ *
+ */
+
+import React from "react";
+
+import styles from "./styles.css";
+
+function Navigation() {
+  return (
+    <div className={styles.navigation}>This is the navigation component</div>
+  );
+}
+
+export default Navigation;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,17 +9,12 @@
  * the linting exception.
  */
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-
-export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+import React from "react";
+import NavigationContainer from "../../containers/NavigationContainer";
+export default class HomePage extends React.Component {
+  // eslint-disable-line react/prefer-stateless-function
 
   render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
+    return <NavigationContainer />;
   }
 }
