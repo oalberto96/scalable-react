8cb1978867957746bff242db62dcdca96b7c40f1
oalberto96 <ochoapublic@gmail.com>
2018-09-13 22:47:00 -0700

    Adding an Additional Container component to an existing route

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,10 +11,17 @@
 
 import React from "react";
 import NavigationContainer from "../../containers/NavigationContainer";
+import LinkListContainer from "../../containers/LinkListContainer";
+
 export default class HomePage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
 
   render() {
-    return <NavigationContainer />;
+    return (
+      <div>
+        <NavigationContainer />
+        <LinkListContainer />
+      </div>
+    );
   }
 }
